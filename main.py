0baf84498507fd09d91f22f59deeb59644113fa8
Bismarck Ng <bismarck4game@gmail.com>
2025-10-02 09:54:59 +0800

    feat(player): create player moving speed

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ pygame.init()
 black = (0, 0, 0)  # Black
 green = (0, 255, 0)  # Green
 
+
+# Player class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -23,23 +25,24 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(green)  # Green spaceship
         self.rect = self.image.get_rect()
         self.rect.center = (width // 2, height // 2)
+        self.speed = 4
 
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.rect.x -= 5
+            self.rect.x -= self.speed
             if self.rect.left < 0:
                 self.rect.left = 0
         if keys[pygame.K_RIGHT]:
-            self.rect.x += 5
+            self.rect.x +=   self.speed
             if self.rect.right > width:
                 self.rect.right = width
         if keys[pygame.K_UP]:
-            self.rect.y -= 5
+            self.rect.y -= self.speed
             if self.rect.top < 0:
                 self.rect.top = 0   
         if keys[pygame.K_DOWN]:
-            self.rect.y += 5
+            self.rect.y += self.speed
             if self.rect.bottom > height:
                 self.rect.bottom = height
 

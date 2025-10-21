70bdc482b166940c5b40a992f65bb9ed8c6c4be8
Bismarck Ng <bismarck4game@gmail.com>
2025-10-21 09:26:17 +0800

    refactor(stone-class): create stone.py for stone object

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import subprocess
 from color import *
 from game_image_loader import load_assets
 
+# Import classes
+from stone import Stone
+
 
 game_mode = "single_player_pve" # default mode
 game_mode_id = 0
@@ -279,44 +282,6 @@ class Explosion(pygame.sprite.Sprite):
                 self.rect = self.image.get_rect()
                 self.rect.center = center
 
-# Stone class 
-class Stone(pygame.sprite.Sprite):
-    def reset_position(self):
-        # some stones may appear partially off-screen
-        self.rect.x = random.randint(-100, width - self.rect.width+100) 
-        self.rect.y = random.randint(-100, -40)
-        self.speedy = random.randint(1,6)
-        self.speedx = random.randint(-2, 2)
-    def __init__(self):
-        super().__init__()
-        self.image_ori = random.choice(stone_img)
-        self.image_ori.set_colorkey(black)
-        self.image = self.image_ori.copy()
-        self.total_degree = 0
-        self.rot_degree = random.randrange(-3, 3)
-        self.size = random.randint(20,100)
-        # self.image = pygame.Surface((self.size, self.size))
-        # self.image.fill(brown)  # Brown stone
-        self.rect = self.image.get_rect()
-        self.radius = self.rect.width * 0.85 /2
-        # pygame.draw.circle(self.image,red,self.rect.center,self.radius) #Debug rendering
-        self.reset_position()    
-
-    def update(self):
-        self.rotate()
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        if self.rect.top > height:
-            self.reset_position() 
-
-    def rotate(self):   
-        self.total_degree += self.rot_degree
-        self.total_degree = self.total_degree % 360
-        self.image = pygame.transform.rotate(self.image_ori, self.total_degree)
-        center = self.rect.center
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-
 # Bullet class
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, type):
@@ -352,7 +317,7 @@ if game_mode_id == 1:  # multiplayer mode
 all_sprites.add(players_group)
  
 for _ in range(8):
-    stone = Stone()
+    stone = Stone(stone_img,width,height)
     all_sprites.add(stone)
     stones.add(stone)
 

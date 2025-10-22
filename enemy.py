c1628bbf34ff8ca62fdbf549fcbad6fc581933fc
BismarckKL <bismarck4game@gmail.com>
2025-10-22 19:43:43 +0800

    feat: adjust data and set add_score callback

--- enemy.py
+++ enemy.py
@@ -7,17 +7,20 @@ enemy_images = enemy_img_loader()
 
 # Enemy types
 enemy_types = {
-    'basic': { 'size': (50, 50), 'health_point': 1, 'speed': 2, 'image': enemy_images[0]},
-    'fast': {'size': (50, 50), 'health_point': 1, 'speed': 4, 'image': enemy_images[1]},
-    'strong': {'size': (100, 100), 'health_point': 3, 'speed': 0.5, 'image': enemy_images[2]}
+    'basic': { 'size': (50, 50), 'health_point': 3, 'speed': 2, 'image': enemy_images[0], 'score': 20},
+    'fast': {'size': (50, 50), 'health_point': 1, 'speed': 4, 'image': enemy_images[1], 'score': 20},
+    'strong': {'size': (100, 100), 'health_point': 10, 'speed': 1, 'image': enemy_images[2], 'score': 50}
 }
 
 # Enemy class
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height, enemy_type):
+    def __init__(self, screen_width, screen_height, enemy_type, score_callback=None):
         super().__init__()
         self.screen_width = screen_width
         self.screen_height = screen_height
+
+        self.score = enemy_types[enemy_type]['score']
+        self.score_callback = score_callback
 
         self.image = pygame.transform.scale(enemy_types[enemy_type]['image'], enemy_types[enemy_type]['size'])
         self.image.set_colorkey(white)
@@ -25,7 +28,7 @@ class Enemy(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, screen_width - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-50, -40)
         self.speedy = enemy_types[enemy_type]['speed']
         self.health_point = enemy_types[enemy_type]['health_point']
 
@@ -33,9 +36,12 @@ class Enemy(pygame.sprite.Sprite):
         # move downwards
         self.rect.y += self.speedy
         if self.rect.top > self.screen_height:
-            self.kill()  # Remove the enemy if it goes off the screen
+            # reset position to top
+            self.rect.y = random.randrange(-50, -40)
+            self.rect.x = random.randrange(0, self.screen_width - self.rect.width)
 
     def update_health_point(self, hp):
         self.health_point += hp
         if self.health_point <= 0:
+            self.score_callback(self.score)
             self.kill()  # Remove the enemy if health is 0 or less

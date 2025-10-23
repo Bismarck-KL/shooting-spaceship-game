89549363dacf24611e54bdc22cf8b201efb8d8af
Bismarck Ng <bismarck4game@gmail.com>
2025-10-23 11:00:17 +0800

    feat(color-util): add ui color

--- utils/color.py
+++ utils/color.py
@@ -9,3 +9,6 @@ star_color = (128,128,128)
 blue = (0, 0, 255)  # Blue
 gray = (128, 128, 128)  # Gray
 orange = (255, 165, 0)  # Orange
+blue = (0, 128, 255)  # Default color
+btn_hover_color = (0, 160, 255)  # Lighter blue when hovered
+btn_active_color = (0, 100, 200)  # Darker blue when active
